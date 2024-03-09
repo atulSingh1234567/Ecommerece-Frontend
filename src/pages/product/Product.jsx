d2303a8af946c8a089f57d85f214b2144f7fa9b7
Atul Singh <sahajkrish7@gmail.com>
2024-03-10 00:18:08 +0530

    filter product added

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -5,18 +5,21 @@ import { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Pagination } from '@mui/material';
 
+
 export default function Product() {
-  const brands = [`Levi's`, `Adidas`, `Puma`, `H&M`, `Other`]
   const { category } = useParams()
   // console.log(category)
   const [cardImg, setCardImg] = useState([]);
   const [countProd, setCountProd] = useState(0);
+  const [priceRange , setPriceRange] = useState(10000);
+  const [rating , setRating] = useState('0')
 
+  const rgx = /[1-5]/g;
   const pages = Math.ceil(countProd / 50)
   useEffect(
     () => {
-      if (category !== 'best deals') {
-        axios.post('https://ecommerece-backend.vercel.app/api/v1/products?pageno=1', { category: category })
+       if (category !== 'best deals') {
+        axios.post(`https://ecommerece-backend.vercel.app/api/v1/products?pageno=1`, { category: category })
           .then(
             (response) => {
               setCardImg(response.data.products);
@@ -32,7 +35,7 @@ export default function Product() {
           )
       }
       else {
-        axios.get('https://ecommerece-backend.vercel.app/api/v1/best-deals?pageno=1')
+        axios.get(`https://ecommerece-backend.vercel.app/api/v1/best-deals?pageno=1`)
           .then(
             (res) => {
               setCardImg(res.data.actualProd)
@@ -46,6 +49,8 @@ export default function Product() {
             }
           )
       }
+     
+     
     }, []
   )
 
@@ -81,31 +86,48 @@ export default function Product() {
       }
   }
 
+  const filtering = (br)=>{
+      if(br === 'All' || br == undefined){
+        const filteredPrd =  cardImg
+        .filter(function (item) {
+          return item.price < parseInt(priceRange) && item.rating > parseInt(rating);
+        });
+        return filteredPrd;
+      }else{
+        const filteredPrd =  cardImg
+        .filter(function (item) {
+          return item.price < parseInt(priceRange) && item.rating > parseInt(rating) && item.brand === br;
+        });
+        return filteredPrd;
+      }
+  
+  }
+
+
   return (
     <div className='flex wrap relative justify-evenly w-screen top-20'>
       <div className='flex h-fit flex-col gap-4 w-1/6 bg-white p-3'>
         <div className='flex flex-col gap-2'>
-          <h1 className='text-xl'>Brand</h1>
-          <ul className='flex flex-col gap-1'>
-            {
-              brands.map(function (item, index) {
-                return <li key={index}> <InputBox inputFor={item} type='checkbox' /> </li>
-              })
-            }
+          <h1 className='text-xl'>Ratings</h1>
+          <ul className='flex flex-col gap-1 cursor-pointer gap-1'>
+            <li onClick={(e)=>setRating((e.target.innerText).match(rgx))} className='text-blue-600 border px-2 rounded'>above 4</li>
+            <li onClick={(e)=>setRating((e.target.innerText).match(rgx))} className='text-blue-600 border px-2 rounded'>above 3</li>
+            <li onClick={(e)=>setRating((e.target.innerText).match(rgx))} className='text-blue-600 border px-2 rounded'>above 2</li>
           </ul>
         </div>
         <div className='flex flex-col gap-2'>
           <h1 className='text-xl'>Price</h1>
-          <InputBox type='range' minPrice={100} maxPrice={5000} step={100} />
+          <input type='range' min={10} max={1000} step={10} value={priceRange} onChange={(e)=>setPriceRange(e.target.value)} />
+          <h1>Less than <span className='text-green-500 font-semibold'>${priceRange}</span></h1>
         </div>
       </div>
       <div className='w-4/5 p-2 bg-white'>
         <h1 className='text-xl pb-4'>{category}</h1>
         <div className='flex flex-wrap gap-2'>
           {
-            cardImg.map(function (item, index) {
-              return <Card key={item._id} item={item}  goto={`checkout/${item.productname}`} imgSrc={item.img} name={item.productname} price={item.price} info={`upto ${Math.floor(item.discount)}% off`} rating={item.rating} prodId={item._id} />
-            })
+              filtering().map(function(item,index){
+                      return <Card key={item._id} item={item}  goto={`checkout/${item.productname}`} imgSrc={item.img} name={item.productname} price={item.price} info={`upto ${Math.floor(item.discount)}% off`} rating={item.rating} prodId={item._id} />
+              })
           }
         </div>
         {
